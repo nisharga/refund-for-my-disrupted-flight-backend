25d9d938ec7f662cb6b054560d170bcde0cbcf29
Nisharga Kabir <kabirnisharga@gmail.com>
2023-06-21 14:25:51 +0600

    [done] mongoDB connect use mongoose and test route setup

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { port } = require("./config");
+const { connectToDatabase } = require("./server/connectToDatabase");
 const app = express();
 
-const port = process.env.PORT || 5000;
-require("dotenv").config();
+const testRoute = require("./routes/testRoute.js");
 
 //default middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
+//mongoDB connect with mongoose
+connectToDatabase();
+
+//Routes
+app.use("/api/v1/", testRoute);
+
 app.get("/", (req, res) => {
-  res.send("I Love Express");
+  res.send(`I love you Express, my port is ${port}`);
 });
 
 app.listen(port, () => {
   console.log(`port listen, ${port}`);
 });
+
+module.exports = app;

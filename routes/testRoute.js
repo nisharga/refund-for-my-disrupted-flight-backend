43de3443c0952abdbc029e2f46e972d487a247be
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-06-23 02:40:06 +0200

    final prompt and route change

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route("/test").get((req, res) => {
   res.send("Our Route is ON");
 });
-router.route("/response").get(getResponse);
-router.route("/response").post(postResponse);
+router.route("/test").get(getResponse);
+router.route("/eligibile").post(postResponse);
 
 module.exports = router;
